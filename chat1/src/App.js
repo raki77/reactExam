edc8786297293db8f754cdac2435fc4882144d6e
raki77 <rakisiss77@gmail.com>
2025-03-27 16:05:48 +0900

    250327-04

---
+++ chat1/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+function App() {
+
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+  let timeoutID = null;
+  
+  function handleSend() {
+    setIsSending(true);
+    timeoutID = setTimeout(() => {
+      alert('Sent!');
+      setIsSending(false);
+    }, 3000);
+  }
+
+  function handleUndo() {
+    setIsSending(false);
+    clearTimeout(timeoutID);
+  }
+
+  return (
+    <>
+      <input
+        disabled={isSending}
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <button 
+        disabled={isSending}
+        onClick={handleSend}>
+        {isSending? 'Sending...' : 'Send'}
+      </button>
+      {isSending &&
+        <button onClick={handleUndo}>
+          Undo
+        </button>
+      }
+    </>
+  );
+}
+
+export default App;
